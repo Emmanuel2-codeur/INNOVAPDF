ab9809db13bc39fdcefd2c8e4462b8c415c1a92a
Emmanuel de track <eadande2@gmail.com>
2026-09-07 23:33:59 +0000

    fix: simplification de bootstrap.ts sans axios

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,4 +1,11 @@
+import '../css/app.css';
+import './bootstrap';
+
 import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createPinia } from 'pinia';
+import { createApp, h, type DefineComponent } from 'vue';
+
 import { initializeTheme } from '@/composables/useAppearance';
 import AppLayout from '@/layouts/AppLayout.vue';
 import AuthLayout from '@/layouts/AuthLayout.vue';
@@ -6,28 +13,38 @@ import SettingsLayout from '@/layouts/settings/Layout.vue';
 import { initializeFlashToast } from '@/lib/flashToast';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const pinia = createPinia();
 
 void createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>('./Pages/**/*.vue')
+        ),
     layout: (name) => {
         switch (true) {
             case name === 'Welcome':
                 return null;
-            case name.startsWith('auth/'):
+            case name.startsWith('auth/') || name.startsWith('Auth/'):
                 return AuthLayout;
-            case name.startsWith('settings/'):
+            case name.startsWith('settings/') || name.startsWith('Settings/'):
                 return [AppLayout, SettingsLayout];
             default:
                 return AppLayout;
         }
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(pinia)
+            .mount(el);
     },
     progress: {
         color: '#4B5563',
     },
 });
 
-// This will set light / dark mode on page load...
+// Initialisations au chargement
 initializeTheme();
-
-// This will listen for flash toast data from the server...
 initializeFlashToast();
