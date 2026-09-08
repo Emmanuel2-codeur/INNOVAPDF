92d453dd3e74f85ff7c67cee94fbbefedae733f8
Emmanuel de track <eadande2@gmail.com>
2026-09-08 00:07:58 +0000

    fix: ajout de l'import HasMany manquant dans le modèle User

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany; // <-- LIGNE AJOUTÉE ICI
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
@@ -47,7 +48,8 @@ class User extends Authenticatable implements PasskeyUser
             'two_factor_confirmed_at' => 'datetime',
         ];
     }
-     public function documents(): HasMany
+    
+    public function documents(): HasMany
     {
         return $this->hasMany(Document::class);
     }
